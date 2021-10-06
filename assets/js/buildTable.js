9fc488b88f9b1bc263449452d2225c6a97749a0a
TechnoME <Phillip.TechnoME@t-online.de>
2021-10-06 16:32:12 +0200

    updated webpage and buildTable function

--- assets/js/buildTable.js
+++ assets/js/buildTable.js
@@ -1,4 +1,27 @@
-function buildTable(vars) {
-    var test = JSON.parse(vars)
-    console.log(test)
+function buildTable(data, table) {
+    var tbl = document.getElementsByTagName("table")[0]
+    var week = JSON.parse(data)
+
+    for (let i = 0; i < week.length; i++) {
+        var day = week[i]
+        var col = document.createElement("td")
+        for (let j = 0; j < day.length; j++) {
+            var lesson = day[j]
+            var cell = document.createElement("tr");
+            
+            if (lesson.length == 1) {
+                if (lesson == "empty") {
+                    var cellText = document.createTextNode("-");
+                    cell.setAttribute("style","color:white")
+                }else{
+                    var cellText = document.createTextNode(lesson);
+                }
+            }else if (lesson.length == 2) {
+                var cellText = document.createTextNode(lesson);
+            }
+            cell.appendChild(cellText);
+            col.appendChild(cell);
+        }
+        tbl.appendChild(col);
+    }
 }
