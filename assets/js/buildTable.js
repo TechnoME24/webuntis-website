abf332625d8de2548f49c962ebf5f0c5b3bbd3b6
TechnoME <Phillip.TechnoME@t-online.de>
2021-10-07 13:57:55 +0200

    unfinished

--- assets/js/buildTable.js
+++ assets/js/buildTable.js
@@ -1,11 +1,38 @@
-function buildTable(data, table) {
-    var week = JSON.parse(data)
-    var body = document.getElementsByTagName("body")[0]
+function buildTable(data) {
+    let week = JSON.parse(data)
+    
+    //important variables
+    let div = document.getElementsByClassName("createTable")[0]
+    let table = document.createElement("table")
+    let thead = document.createElement("thead")
+    let tbody = document.createElement("tbody")
+    let firstRow = document.createElement("tr")
+    let firstCell = document.createElement("th")
+    
+    //set atributes
+    table.setAttribute("class", "table table-striped")
 
+    //create Headline
+    firstCell.appendChild(document.createTextNode("#"))
+    firstCell.setAttribute("scope","col")
+    firstRow.appendChild(firstCell)
     for (let i = 0; i < week.length; i++) {
-        var h2 = document.createElement("h2")
-        h2.appendChild(document.createTextNode(week[i]))
-        body.appendChild(h2)
+        let colText = document.createTextNode(week[i][0])
+        let col = document.createElement("th")
+        col.setAttribute("scope","col")
+        col.appendChild(colText)
+        firstRow.appendChild(col)
     }
+    thead.appendChild(firstRow)
+    table.appendChild(thead)
 
+    //create Table
+    for (let j = 1; j < week[0].length; j++) {
+        for (let i = 0; i < week.length; i++) {
+            
+        }
+    }
+    table.appendChild(tbody)
+
+    div.appendChild(table)
 }
